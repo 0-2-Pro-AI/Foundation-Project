e39cc260486eb9da072eb4f498f8925c922e3fbb
GOAT <tongng219@gmail.com>
2025-11-21 01:59:59 +0000

    variable_box

---
+++ python-basic/python ex/client.py
@@ -0,0 +1,16 @@
+ten_khach = "Tong Nguyen"
+tuoi = 22
+tien_trong_vi = 50.5
+
+print("Ten khach hang:", ten_khach)
+print("Tuoi:", tuoi)
+print("So du tai khoan:", tien_trong_vi)
+
+tien_nap_them = 20
+tong_tien = tien_trong_vi + tien_nap_them
+
+print("Tong tien sau khi nap:", tong_tien)
+
+print(type(ten_khach))
+print(type(tuoi))
+print(type(tien_trong_vi))
