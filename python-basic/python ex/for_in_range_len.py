fd3d2870f1427dda66fcbb0d7c2193f54f3cd109
GOAT <tongng219@gmail.com>
2025-11-22 22:34:27 +0000

    new_tool_len_is_length

---
+++ python-basic/python ex/for_in_range_len.py
@@ -0,0 +1,29 @@
+food_list = ["Pizza", "Kebab", "KFC", "Donut", "Chips"]
+price_list = [2, 3.4, 1.3, 9.3, 5.6]
+status_list = ["conclude","pendent","cancel","conclude","pendent"]
+
+print("---Revenue of Send2You---")
+total_revenue = 0
+total_waitlist = 0
+max_price = 0
+
+
+for i in range(len(food_list)):
+    food = food_list[i]
+    price = price_list[i]
+    status = status_list[i]
+
+    if status == "conclude":
+        total_revenue = total_revenue + price
+
+    if status == "pendent":
+        total_waitlist = total_waitlist + 1
+
+    if price > max_price:
+        max_price = price
+
+print("-----------------")
+print(".....Total revenue.....")
+print(total_revenue)
+print(total_waitlist)
+print(max_price)
