9d820f1b303673ce61111e465fa3aec05014d5ae
GOAT <tongng219@gmail.com>
2025-11-22 01:00:11 +0000

    register_facebook_name

---
+++ python-basic/python ex/facebook.py
@@ -0,0 +1,12 @@
+guest_list = []
+print("Welcome to our list.")
+
+for i in range(3):
+    new_name = input("Please enter your name: ")
+    guest_list.append(new_name)
+
+    print("You successfully registered")
+
+print("---Final list---")
+print(guest_list)
+
