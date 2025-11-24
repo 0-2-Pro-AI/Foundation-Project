c18accff7f4944764f41a6e1f9d34ac0c882c261
GOAT <tongng219@gmail.com>
2025-11-24 19:32:09 +0000

    TD2CE

---
+++ python-basic/python ex/project_Send2You_parte1.py
@@ -0,0 +1,53 @@
+food_list = []
+status_list = []
+
+def MENU():
+    print("\n"+"="*30)
+    print("   SEND2YOU - CLIENT GATE   ")
+    print("="*30)
+    print("1. Make your new order")
+    print("2. Check your listing")
+    print("3. Exit")
+
+def make_your_new_order():
+    print("\n---- MAKING YOUR NEW ORDER ----")
+    
+    food = input("Your order: ")
+    address = input("Delivery address: ")
+
+    information_of_client = f"{food} (at {address})"
+    food_list.append(information_of_client)
+
+    status_list.append("Pendent")
+    print(f"Success! {food} order is in status `Pendent`.")
+
+def check_your_listing():
+    print("\n ---- MY ORDERS ----")
+
+    if len(food_list) == 0:
+        print("No orders found.")
+
+    else:
+        for i in range(len(food_list)):
+            print(f"ID {i+1} | {food_list[i]} | Status: {status_list[i]}")
+            
+
+def main():
+    processing = True
+
+    while processing == True:
+        MENU()
+        choices = input("Please enter (1/2/3): ")
+
+        if choices == "1":
+            make_your_new_order()
+        elif choices == "2":
+            check_your_listing()
+        elif choices == "3":
+            print("Good bye!")
+
+        else:
+            print("Your choice is invalid! Please try again!")
+
+main()
+
