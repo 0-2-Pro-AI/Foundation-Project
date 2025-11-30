d52a5f3b7a1954870dad28575278b357eca485a6
GOAT <tongng219@gmail.com>
2025-11-30 22:59:01 +0000

    gemini_debug_send2u

--- python-basic/pythonMini/Send2U_self.py
+++ python-basic/pythonMini/Send2U_self.py
@@ -1,4 +1,4 @@
-orders = ()
+orders = []
 MENU_items = {
     "pizza": 3,
     "kebab": 4,
@@ -13,39 +13,43 @@ def MENU():
     print("3. Exit")
 
 def order():
+    print("\n---MAKING YOUR NEW ORDER--- ")
+    food_name = ""
+    price = 0
     while True:
-        print("\n---MAKING YOUR NEW ORDER--- ")
-        food_name = input("Your order: ").lower
-        if food_name in MENU_items():
-            print(f"Success! Your {food_name} is {MENU_items[food_name]} euros")
+        food_name = input("Your order: ").strip().lower()
+        if food_name in MENU_items:
+            price = MENU_items[food_name]
+            print(f"Success! Your {food_name} is {price} euros")
             break
         else:
-            print("We don`t have this option!")
+            print("We don`t have this option!Please try: ", list(MENU_items.keys()))
 
-        address = input("Delivery address: ")
-        duration = input("Delivery time: ")
+    address = input("Delivery address: ")
+    duration = input("Delivery time: ")
 
     single_order = {
         "food": food_name,
-        "price": MENU_items[food_name],
+        "price": price,
         "address": address,
         "duration": duration,
         "status": "Pendent"
     }
     orders.append(single_order)
+    print("--> Orders saved successfully!")
 
 def checking():
-    print("---Your listings---" )
+    print("\n---Your listings---" )
 
     if not orders:
         print("No order found")
     else:
         for i, order in enumerate(orders,1):
-            print(f"ID: {i} | Your order:{order["food"]} | Price:{order["price"]} | Status:{order["Pendent"]}")
+            print(f"ID: {i} | Your order:{order['food']} | Price:{order['price']} | Status:{order['status']}")
 
 def main():
     processing = True
-    while processing == True:
+    while processing:
         MENU()
         choice = input("Choose (1/2/3): ")
 
